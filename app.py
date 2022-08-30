e8c52a5879aad39e7d7c0546e9d80216aca6c199
Teun Schaeken <teun.schaeken@gmail.com>
2022-08-30 18:18:14 +0200

    first idea

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+from dataclasses import dataclass
 import streamlit as st 
+import pandas as pd 
 
+class Opportunity:
+    def __init__(self, info: dict) -> None:
+        self.link: str = info['link']
+        self.company: str = info['company']
+
+df = pd.read_excel("data/opportunities.xlsx")
+opportunities = [Opportunity(row) for _, row in df.iterrows()]
 st.write("hello world!")
+st.write(opportunities)
